d4916b2922efb47298479e3c87d1f00c3d76d181
Will <will@Zipcoders-MacBook-Pro-29.local>
2024-09-20 12:27:05 -0400

    finished java problem

---
+++ src/main/java/org/example/MergeStringsAlternately.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class MergeStringsAlternately {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+
+
+        for (int i = 0; i < word1.length() || i < word2.length(); i++) {
+
+
+            if (i < word1.length())
+                result.append(word1.charAt(i));
+
+
+            if (i < word2.length())
+                result.append(word2.charAt(i));
+        }
+
+        return result.toString();
+    }
+}
