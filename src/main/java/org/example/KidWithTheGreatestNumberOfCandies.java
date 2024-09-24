08bdcc237aee7a229250c6aaba92f90b15cf13de
Will <will@Zipcoders-MacBook-Pro-29.local>
2024-09-24 15:35:23 -0400

    solved java problem

---
+++ src/main/java/org/example/KidWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.List;
+
+public class KidWithTheGreatestNumberOfCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        Boolean [] result = new Boolean[candies.length];
+        int maxCandies = 0;
+
+        for(int i=0; i<candies.length; i++) {
+
+            if (candies[i] > maxCandies) {
+                maxCandies = candies[i];
+
+            }
+            System.out.println(maxCandies);
+            for (int j = 0; j < candies.length; j++) {
+
+                if (candies[j] + extraCandies >= maxCandies) {
+                    result[j] = true;
+                } else {
+                    result[j] = false;
+                }
+
+            }
+        }
+        return List.of(result);
+    }
+}
