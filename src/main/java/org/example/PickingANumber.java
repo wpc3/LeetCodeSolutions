3efa00377a6cff8eb12725c8334d3cec030a4744
wpc3 <williamchapman53@gmail.com>
2024-11-21 11:37:37 -0500

    solved hacker problem

---
+++ src/main/java/org/example/PickingANumber.java
@@ -0,0 +1,71 @@
+package org.example;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class PickingANumber {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) {
+        // Write your code here
+
+
+        Collections.sort(a);
+        int l = 0;
+        int r = 1;
+        int score = 0;
+
+        while( l<r && r < a.size() ){
+            if (a.get(r) - a.get(l) <= 1){
+                score = Math.max(score , r - l + 1);
+            }else{
+                l = r;
+            }
+            r++;
+        }
+
+        return score;
+
+
+
+
+
+    }
+
+}
+
+//public class Solution {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//
+//        int n = Integer.parseInt(bufferedReader.readLine().trim());
+//
+//        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+//                .map(Integer::parseInt)
+//                .collect(toList());
+//
+//        int result = PickingANumber.pickingNumbers(a);
+//
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedReader.close();
+//        bufferedWriter.close();
+//    }
+//}
